bccb12d4cf200b4a99c242c55e4539a93ea00be2
Shivendra99 <saxenashivendra5@gmail.com>
2024-05-08 21:51:17 +0530

    update

--- src/main/java/com/example/demo/EquationController.java
+++ src/main/java/com/example/demo/EquationController.java
@@ -3,10 +3,12 @@ package com.example.demo;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 @Controller
+@CrossOrigin
 public class EquationController {
 
     @PostMapping("/getEquation")
